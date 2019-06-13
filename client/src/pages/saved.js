4ffe9e9722b216289f3b219e2fcf800d8b5d7850
candye_sweet <olya.melska@gmail.com>
2019-06-13 16:50:35 -0400

    Adding content to saved books page

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/header";
+import Book from "../components/book";
+import DeleteBtn from "../components/deleteBtn";
+import Main from "../components/main";
 
-const Saved = ()=>{
-    return(
-        <div>
-       <Header />
-            Saved
-        </div>
-    )
-}
+const Saved = () => {
+  const [books, setBooks] = useState([]);
+  const [err, setErr] = useState();
+
+  const handleClick = e => {
+    e.preventDefault();
+  };
+
+  const setData = data => {
+    console.log("DATA ", data);
+    data.forEach(element => {
+      setBooks([
+        ...books,
+        {
+          title: element.title,
+          authors: element.authors,
+          description: element.description,
+          img: element.image,
+          alt: element.title + "-img",
+          link: element.link
+        }
+      ]);
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get("/api/saved/books")
+      .then(res => {
+        if (res.length === 0) {
+          throw new Error("No results found.");
+        }
+        if (res.status === "error") {
+          throw new Error(res.data.message);
+        }
+        setData(res.data);
+      })
+      .catch(err => setErr(err));
+  }, [err]);
+
+  return (
+    <div>
+      <Header />
+      <Main>
+        {books.map(book => (
+          <Book
+            key={book._id}
+            title={book.title}
+            url={book.img}
+            alt={book.alt}
+            authors={book.authors}
+            description={book.description}
+            link={book.link}
+          >
+            <DeleteBtn />
+          </Book>
+        ))}
+      </Main>
+    </div>
+  );
+};
 
 export default Saved;
--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,75 +1,86 @@
-import React, {useState, useEffect, useContext, useCallback} from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import Header from "../components/header";
 import Main from "../components/main";
 import "./style.css";
 import zoom from "../assets/images/zoom.png";
 import API from "../utils/API";
 import Book from "../components/book";
-import globalContext from "../components/context"
+import globalContext from "../components/context";
 import GlobalContext from "../components/context";
 import SaveBtn from "../components/saveBtn";
 
+const Search = () => {
+  const [searchedBook, setSearch] = useState("");
+  const [books, setBooks] = useState([]);
+  const [err, setErr] = useState();
 
-const Search = ()=>{
-    const [searchedBook, setSearch]  = useState("");
-    const [books, setBooks] = useState([]);
-    const [err, setErr] = useState();
- 
+  const handleClick = e => {
+    e.preventDefault();
+  };
 
-    const handleClick = e =>{
-        e.preventDefault();
-    }
+  const setData = data => {
+    console.log("DATA ", data.items);
+    data.items.forEach(element => {
+      setBooks([
+        ...books,
+        {
+          title: element.volumeInfo.title,
+          authors: element.volumeInfo.authors,
+          description: element.volumeInfo.description,
+          img: element.volumeInfo.imageLinks.smallThumbnail,
+          alt: element.volumeInfo.title + "-img",
+          link: element.selfLink
+        }
+      ]);
+    });
+  };
 
-    const setData = (data) => {
-        console.log("DATA ", data.items)
-        data.items.forEach(element=>{
-            setBooks([...books, {
-                title: element.volumeInfo.title,
-                authors: element.volumeInfo.authors,
-                description: element.volumeInfo.description,
-                img: element.volumeInfo.imageLinks.smallThumbnail,
-                alt: element.volumeInfo.title+"-img",
-                link: element.selfLink
-            }])
-        })     
-    }
+  useEffect(() => {
+    API.searchBook(searchedBook)
+      .then(res => {
+        if (res.data.length === 0) {
+          throw new Error("No results found.");
+        }
+        if (res.data.status === "error") {
+          throw new Error(res.data.message);
+        }
+        setData(res.data);
+      })
+      .catch(err => setErr(err));
+  }, [searchedBook]);
 
-    useEffect( ()=>{
-        API.searchBook(searchedBook).then(res=>{
-            if (res.data.length === 0) {
-                throw new Error("No results found.");
-              }
-              if (res.data.status === "error") {
-                throw new Error(res.data.message);
-              }
-              setData(res.data);
-             
-        }).catch(err => setErr(err));
-        
-    }, [searchedBook]);
-
-   
-
-    return(
-        <div>
-            <Header/>
-            <form onSubmit={handleClick}>
-            <input type="text" onChange={event=> setSearch(event.target.value)}/>
-            <img src={zoom} alt="zoom-icon" id="zoom-icon" type="submit" onClick={event=>{setSearch(event.target.value)}}/>
-            </form>
-            <Main value={GlobalContext}>
-                {books.map(book=>(
-                    <Book title={book.title} url={book.img} alt={book.alt} authors={book.authors} description={book.description} link={book.link}>
-                        <SaveBtn />
-                    </Book>
-                ))}
-                
-        
-            </Main>
-            
-            </div>
-            
-    )
-}
+  return (
+    <div>
+      <Header />
+      <form onSubmit={handleClick}>
+        <input type="text" onChange={event => setSearch(event.target.value)} />
+        <img
+          src={zoom}
+          alt="zoom-icon"
+          id="zoom-icon"
+          type="submit"
+          onClick={event => {
+            setSearch(event.target.value);
+          }}
+        />
+      </form>
+      <Main value={GlobalContext}>
+        {books.map(book => (
+          <Book
+            key={book.title}
+            title={book.title}
+            url={book.img}
+            alt={book.alt}
+            authors={book.authors}
+            description={book.description}
+            link={book.link}
+          >
+            <SaveBtn />
+          </Book>
+        ))}
+      </Main>
+    </div>
+  );
+};
 
 export default Search;
